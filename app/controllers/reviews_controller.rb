4f0c3a2b3023f412bbe817b3f881228e76656c46
Matthieu JEGO <jego_matt@hotmail.com>
2018-11-21 16:03:55 +0100

    mi exo2

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :index, :create]
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @review = Review.new
+  end
+  def index
+    @reviews = @restaurant.reviews
+
+  end
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = @restaurant
+    @review.save
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "controller#action"
+    resources :restaurants do
+      resources :reviews, only: [ :new, :create, :show, :index]
+      end
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants" , to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants/:id/reviews/new"
+  # post "restaurants/:id/reviews"
+
+  root to: "restaurants#index"
 end
